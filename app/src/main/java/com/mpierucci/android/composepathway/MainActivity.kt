04a881014c505fb55375d2c1ac121120f3741bc5
Marco <marco1pierucci@gmail.com>
2020-10-02 23:46:04 +0100

    FIrst Compose lessons

--- app/src/main/java/com/mpierucci/android/composepathway/MainActivity.kt
+++ app/src/main/java/com/mpierucci/android/composepathway/MainActivity.kt
@@ -2,11 +2,14 @@ package com.mpierucci.android.composepathway
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.mpierucci.android.composepathway.ui.ComposePathWayTheme
 
@@ -14,25 +17,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePathWayTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NewStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewStory() {
+    val image = imageResource(id = R.drawable.header)
+    Column(modifier = Modifier.padding(8.dp)) {
+        val imageModifier = Modifier.preferredHeight(180.dp)
+            .fillMaxWidth()
+        Image(image, modifier = imageModifier)
+        Spacer(modifier = Modifier.preferredHeight(16.dp))
+        Text("A what a wonderful day")
+        Text("To learn compose")
+        Text("Following the path way")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePathWayTheme {
-        Greeting("Android")
+        NewStory()
     }
 }
